4022a5585fb0a03e69e98876e355696430e0e181
Sławomir Grzebień <grzebyk@Grzebien.local>
2017-03-03 17:34:18 +0100

    project ended

---
+++ Assets/ChangeShader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShader : MonoBehaviour {
+
+	public static Renderer rend;
+	private static float n;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("MyCustomShader");
+//		rend.material.SetColor("_Color", new Color (1.0f,0.0f,0.0f,0.5f));
+		n = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		this.rend.material.SetColor("_Color", new Color (1.0f,0.0f,0.0f,0.5f));
+	}
+
+	public static void ChangeShade(){
+		rend.material.SetColor ("_Color", new Color (1.0f, 0.0f+n, 0.0f, 0.5f));
+		n += 0.2f;
+	}
+
+
+//	void OnGUI(){
+//
+//		if (GUI.Button (new Rect (10, 10, 150, 100), "Change")) {
+//			print ("You clicked the button!");
+//			//				_EpcDevice = new EpcDevice (host, port, deviceName, dataID, sensorName);
+//			this.rend.material.SetColor ("_Color", new Color (1.0f, 0.0f+n, 0.0f, 0.5f));
+//			n += 0.2f;
+//		}
+//	}
+}
---
+++ Assets/MainScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+//	private ChangeShader shader;
+
+	private GameObject tripod1;
+	private GameObject tripod2;
+
+	// Use this for initialization
+	void Start () {
+//		shader = GetComponent<ChangeShader>();	
+		tripod1 = GameObject.Find ("Tripod1");
+		tripod2 = GameObject.Find ("Tripod2");
+
+		tripod1.SetActive (true);
+		tripod1.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect (10, 10, 150, 100), "Change")) {
+			print ("You clicked the button!");
+			//				_EpcDevice = new EpcDevice (host, port, deviceName, dataID, sensorName);
+//			ChangeShader.ChangeShade ();
+			tripod1.SetActive (!tripod1.activeSelf);
+			tripod2.SetActive (!tripod2.activeSelf);
+		}
+	}
+
+}
